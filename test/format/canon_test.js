6f638dbbf84fc7351df7f69f8cc7b0c7aaa8f826
James Coglan <jcoglan@gmail.com>
2026-03-17 21:14:39 +0000

    Create a class for modelling binding contexts

--- test/format/canon_test.js
+++ test/format/canon_test.js
@@ -32,6 +32,13 @@ describe('canon', () => {
       '0000000000000004000000000000000568656c6c6f0000000000000005776f726c6400000000000000016e0000000000000008000000000000002a')
   })
 
+  it('encodes data contained in a Map', () => {
+    let buf = canon.encode(new Map([['hello', 'world'], ['n', 42]]))
+    assert.equal(
+      buf.toString('hex'),
+      '0000000000000004000000000000000568656c6c6f0000000000000005776f726c6400000000000000016e0000000000000008000000000000002a')
+  })
+
   it('sorts multi-valued contexts by key', () => {
     let buf = canon.encode({ n: 42, hello: 'world' })
     assert.equal(
