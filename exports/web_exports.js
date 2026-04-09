212f7653507bab3e810b03d1d82e4060a64aed4a
James Coglan <jcoglan@gmail.com>
2026-04-09 20:06:44 +0100

    Add a IndexedDB-backed adapter

--- exports/web_exports.js
+++ exports/web_exports.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const MemoryAdapter = require('../lib/adapters/memory')
+const IndexedDBAdapter = require('../lib/adapters/indexeddb')
 const WebStorageAdapter = require('../lib/adapters/web_storage')
 const Store = require('../lib/store')
 
 module.exports = {
   MemoryAdapter,
+  IndexedDBAdapter,
   WebStorageAdapter,
   Store
 }
